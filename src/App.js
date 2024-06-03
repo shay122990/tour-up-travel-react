769e540d710c67dde32da041e44513085ed39684
Shay <moeshakasanova@gmail.com>
2024-06-03 12:16:36 +0400

    created  data.js, created media content, created components and relative css, created global css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Hero from "./components/Hero/hero.component";
 import "./index.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Hero />
+    </div>
+  );
 }
 
 export default App;
